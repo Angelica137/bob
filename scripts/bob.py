b4dfb0cfea56dc3fe09dfeb1b77d29c10d871f9f
Angelica <Angelica137@users.noreply.github.com>
2022-07-21 19:32:15 +0100

    bob() returns they know what they doing when yelled a question, green

--- scripts/bob.py
+++ scripts/bob.py
@@ -1,6 +1,10 @@
+from curses.ascii import isupper
+
+
 def bob(statement: str) -> str:
-    # you still do not know if this is all caps
-    if statement[-1] == "?":
+    if statement.isupper() and statement[-1] == "?":
+        return "Calm down, I know what I'm doing!"
+    elif statement[-1] == "?":
         return "Sure."
     elif statement.isupper():
         return "Whoa, chill out!"
