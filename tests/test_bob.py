e069e9bebb22c099828004102219641bafc5869b
Angelica <Angelica137@users.noreply.github.com>
2022-07-21 19:23:28 +0100

    bob() retutns Whoa, chill out if yelled at, red

--- tests/test_bob.py
+++ tests/test_bob.py
@@ -3,3 +3,7 @@ from scripts.bob import bob
 
 def test_input_is_question():
     assert bob("some question?") == "Sure."
+
+
+def test_input_is_yell():
+    assert bob("IS YELLING") == "Whoa, chill out!"
