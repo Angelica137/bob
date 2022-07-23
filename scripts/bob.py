6e42964f3be861bf38edaaabd4e64a18105088b6
Angelica <Angelica137@users.noreply.github.com>
2022-07-23 18:02:21 +0100

    bob() returns fine, be that wa! green

--- scripts/bob.py
+++ scripts/bob.py
@@ -2,11 +2,11 @@ from curses.ascii import isupper
 
 
 def bob(statement: str) -> str:
-    if statement.isupper() and statement[-1] == "?":
+    if statement == "":
+        return "Fine. Be that way!"
+    elif statement.isupper() and statement[-1] == "?":
         return "Calm down, I know what I'm doing!"
     elif statement[-1] == "?":
         return "Sure."
     elif statement.isupper():
         return "Whoa, chill out!"
-    elif statement == "":
-        return "Fine. Be that way!"
